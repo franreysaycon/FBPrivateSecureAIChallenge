f20ef11381570b7ec5699130fd744894205767fb
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2019-06-19 22:11:23 +0800

    Added more part 5

--- part-5/exercise-3.py
+++ part-5/exercise-3.py
@@ -1,3 +1,28 @@
+class Classifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 10)
+
+        # Dropout module with 0.2 drop probability
+        self.dropout = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        # make sure input tensor is flattened
+        x = x.view(x.shape[0], -1)
+
+        # Now with dropout
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.dropout(F.relu(self.fc3(x)))
+
+        # output so no dropout here
+        x = F.log_softmax(self.fc4(x), dim=1)
+
+        return x
+
 model = Classifier()
 criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.003)
@@ -20,11 +45,12 @@ for e in range(epochs):
         running_loss += loss.item()
         
     else:
-         test_loss = 0
+        test_loss = 0
         accuracy = 0
         
         # Turn off gradients for validation, saves memory and computations
         with torch.no_grad():
+            model.eval()
             for images, labels in testloader:
                 log_ps = model(images)
                 test_loss += criterion(log_ps, labels)
@@ -33,7 +59,9 @@ for e in range(epochs):
                 top_p, top_class = ps.topk(1, dim=1)
                 equals = top_class == labels.view(*top_class.shape)
                 accuracy += torch.mean(equals.type(torch.FloatTensor))
-                
+        
+        model.train()
+        
         train_losses.append(running_loss/len(trainloader))
         test_losses.append(test_loss/len(testloader))
 
@@ -41,9 +69,3 @@ for e in range(epochs):
               "Training Loss: {:.3f}.. ".format(running_loss/len(trainloader)),
               "Test Loss: {:.3f}.. ".format(test_loss/len(testloader)),
               "Test Accuracy: {:.3f}".format(accuracy/len(testloader)))
-
-import matplotlib.pyplot as plt
-
-plt.plot(train_losses, label='Training loss')
-plt.plot(test_losses, label='Validation loss')
-plt.legend(frameon=False)
